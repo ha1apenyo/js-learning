9340404caebe860e7b569176e231bac5fd46a5d5
ha1apenyo <danzanyuk2004@gmail.com>
2025-07-22 21:26:33 +0300

    learned concat() method

---
+++ concat-practice.js
@@ -0,0 +1,10 @@
+const frontend = ["HTML", "CSS"];
+const backend = ["Node.js", "MongoDB"];
+const tools = ["Git", "Webpack"];
+
+const fullstack = frontend.concat(backend, tools);
+
+console.log(fullstack);
+console.log(frontend);
+console.log(backend);
+console.log(tools);
